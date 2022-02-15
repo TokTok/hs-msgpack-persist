ae4ba8d86b4f2f665908bcef56618c9007403bee
iphydf <iphydf@users.noreply.github.com>
2022-02-15 00:55:36 +0000

    cleanup: Use Tags from msgpack-types; remove groom.

--- src/Data/MessagePack/Get.hs
+++ src/Data/MessagePack/Get.hs
@@ -31,46 +31,47 @@ import qualified Data.Text.Encoding     as T
 import qualified Data.Vector            as V
 import           Data.Word              (Word16, Word32, Word64, Word8)
 
+import           Data.MessagePack.Tags
 import           Data.MessagePack.Types (Object (..))
 
 getObject :: Get Object
 getObject = getWord8 >>= \case
-  0xC0 -> pure ObjectNil
-  0xC2 -> pure $ ObjectBool False
-  0xC3 -> pure $ ObjectBool True
+  TAG_nil       -> pure ObjectNil
+  TAG_false     -> pure $ ObjectBool False
+  TAG_true      -> pure $ ObjectBool True
   c | c .&. 0xE0 == 0xE0 -> pure $ ObjectInt $ fromIntegral (fromIntegral c :: Int8)
-  0xD0 -> ObjectInt . fromIntegral <$> getInt8
-  0xD1 -> ObjectInt . fromIntegral <$> getInt16be
-  0xD2 -> ObjectInt . fromIntegral <$> getInt32be
-  0xD3 -> ObjectInt . fromIntegral <$> getInt64be
+  TAG_int_8     -> ObjectInt . fromIntegral <$> getInt8
+  TAG_int_16    -> ObjectInt . fromIntegral <$> getInt16be
+  TAG_int_32    -> ObjectInt . fromIntegral <$> getInt32be
+  TAG_int_64    -> ObjectInt . fromIntegral <$> getInt64be
   c | c .&. 0x80 == 0x00 -> pure $ ObjectWord $ fromIntegral c
-  0xCC -> ObjectWord . fromIntegral <$> getWord8
-  0xCD -> ObjectWord . fromIntegral <$> getWord16be
-  0xCE -> ObjectWord . fromIntegral <$> getWord32be
-  0xCF -> ObjectWord . fromIntegral <$> getWord64be
-  0xCA -> ObjectFloat <$> getFloat32be
-  0xCB -> ObjectDouble <$> getFloat64be
+  TAG_uint_8    -> ObjectWord . fromIntegral <$> getWord8
+  TAG_uint_16   -> ObjectWord . fromIntegral <$> getWord16be
+  TAG_uint_32   -> ObjectWord . fromIntegral <$> getWord32be
+  TAG_uint_64   -> ObjectWord . fromIntegral <$> getWord64be
+  TAG_float_32  -> ObjectFloat  <$> getFloat32be
+  TAG_float_64  -> ObjectDouble <$> getFloat64be
   t | t .&. 0xE0 == 0xA0 -> let len = fromIntegral $ t .&. 0x1F in ObjectStr <$> (getByteString len >>= decodeStr)
-  0xD9 -> ObjectStr <$> (fromIntegral <$> getWord8 >>= getByteString >>= decodeStr)
-  0xDA -> ObjectStr <$> (fromIntegral <$> getWord16be >>= getByteString >>= decodeStr)
-  0xDB -> ObjectStr <$> (fromIntegral <$> getWord32be >>= getByteString >>= decodeStr)
-  0xC4 -> ObjectBin <$> (fromIntegral <$> getWord8 >>= getByteString)
-  0xC5 -> ObjectBin <$> (fromIntegral <$> getWord16be >>= getByteString)
-  0xC6 -> ObjectBin <$> (fromIntegral <$> getWord32be >>= getByteString)
+  TAG_str_8     -> ObjectStr <$> (fromIntegral <$> getWord8    >>= getByteString >>= decodeStr)
+  TAG_str_16    -> ObjectStr <$> (fromIntegral <$> getWord16be >>= getByteString >>= decodeStr)
+  TAG_str_32    -> ObjectStr <$> (fromIntegral <$> getWord32be >>= getByteString >>= decodeStr)
+  TAG_bin_8     -> ObjectBin <$> (fromIntegral <$> getWord8    >>= getByteString)
+  TAG_bin_16    -> ObjectBin <$> (fromIntegral <$> getWord16be >>= getByteString)
+  TAG_bin_32    -> ObjectBin <$> (fromIntegral <$> getWord32be >>= getByteString)
   t | t .&. 0xF0 == 0x90 -> let len = fromIntegral $ t .&. 0x0F in ObjectArray <$> V.replicateM len getObject
-  0xDC -> fromIntegral <$> getWord16be >>= \len -> ObjectArray <$> V.replicateM len getObject
-  0xDD -> fromIntegral <$> getWord32be >>= \len -> ObjectArray <$> V.replicateM len getObject
+  TAG_array_16  -> fromIntegral <$> getWord16be >>= \len -> ObjectArray <$> V.replicateM len getObject
+  TAG_array_32  -> fromIntegral <$> getWord32be >>= \len -> ObjectArray <$> V.replicateM len getObject
   t | t .&. 0xF0 == 0x80 -> let len =  fromIntegral $ t .&. 0x0F in ObjectMap <$> V.replicateM len ((,) <$> getObject <*> getObject)
-  0xDE -> fromIntegral <$> getWord16be >>= \len -> ObjectMap <$> V.replicateM len ((,) <$> getObject <*> getObject)
-  0xDF -> fromIntegral <$> getWord32be >>= \len -> ObjectMap <$> V.replicateM len ((,) <$> getObject <*> getObject)
-  0xD4 -> ObjectExt <$> getWord8 <*> getByteString 1
-  0xD5 -> ObjectExt <$> getWord8 <*> getByteString 2
-  0xD6 -> ObjectExt <$> getWord8 <*> getByteString 4
-  0xD7 -> ObjectExt <$> getWord8 <*> getByteString 8
-  0xD8 -> ObjectExt <$> getWord8 <*> getByteString 16
-  0xC7 -> fromIntegral <$> getWord8 >>= \len -> ObjectExt <$> getWord8 <*> getByteString len
-  0xC8 -> fromIntegral <$> getWord16be >>= \len -> ObjectExt <$> getWord8 <*> getByteString len
-  0xC9 -> fromIntegral <$> getWord32be >>= \len -> ObjectExt <$> getWord8 <*> getByteString len
+  TAG_map_16    -> fromIntegral <$> getWord16be >>= \len -> ObjectMap <$> V.replicateM len ((,) <$> getObject <*> getObject)
+  TAG_map_32    -> fromIntegral <$> getWord32be >>= \len -> ObjectMap <$> V.replicateM len ((,) <$> getObject <*> getObject)
+  TAG_fixext_1  -> ObjectExt <$> getWord8 <*> getByteString 1
+  TAG_fixext_2  -> ObjectExt <$> getWord8 <*> getByteString 2
+  TAG_fixext_4  -> ObjectExt <$> getWord8 <*> getByteString 4
+  TAG_fixext_8  -> ObjectExt <$> getWord8 <*> getByteString 8
+  TAG_fixext_16 -> ObjectExt <$> getWord8 <*> getByteString 16
+  TAG_ext_8     -> fromIntegral <$> getWord8    >>= \len -> ObjectExt <$> getWord8 <*> getByteString len
+  TAG_ext_16    -> fromIntegral <$> getWord16be >>= \len -> ObjectExt <$> getWord8 <*> getByteString len
+  TAG_ext_32    -> fromIntegral <$> getWord32be >>= \len -> ObjectExt <$> getWord8 <*> getByteString len
   _ -> fail "Data.MessagePack.Get.getObject: Encountered invalid byte"
 
   where
--- tools/msgpack-gen-sample.hs
+++ tools/msgpack-gen-sample.hs
@@ -1,48 +1,8 @@
 module Main where
 
-import           Control.Monad                    (when)
-import qualified Data.ByteString.Lazy             as L
-import           Data.Int                         (Int64)
-import           Data.MessagePack                 (Object (..), pack)
-import           Data.MessagePack.Arbitrary       ()
-import           Data.Time.Clock                  (diffUTCTime, getCurrentTime)
-import           System.Environment               (getArgs)
-import           System.IO                        (hPutStr, hPutStrLn, stderr)
-import           Test.QuickCheck.Arbitrary        (arbitrary)
-import qualified Test.QuickCheck.Gen              as Gen
-import           Test.QuickCheck.Instances.Vector ()
-
-
-showBytes :: Int64 -> String
-showBytes size
-  | size > 10 * (1024 * 1024) = show (size `div` (1024 * 1024)) <> " MiB"
-  | size > 10 * 1024 = show (size `div` 1024) <> " KiB"
-  | otherwise = show size <> " B"
-
-
-showSpeed :: Int64 -> Double -> String
-showSpeed size time =
-    show (fromIntegral (size `div` (1024 * 1024)) / time) <> " MiB/s"
+import           Data.MessagePack          (pack)
+import           Test.MessagePack.Generate (generate)
 
 
 main :: IO ()
-main = do
-    size:_ <- (++[30]) . map read <$> getArgs
-
-    start <- getCurrentTime
-    hPutStrLn stderr "Generating sample..."
-
-    sample@(ObjectArray array) <- ObjectArray <$> Gen.generate (Gen.resize size arbitrary)
-    when (sample == sample) $  -- force deep evaluation of the whole structure (kind of deepseq)
-        hPutStr stderr $ "Generated msgpack array of length " <> show (length array)
-    sampleTime <- getCurrentTime
-    hPutStrLn stderr $ " in " <> show (diffUTCTime sampleTime start)
-
-    let packed = pack sample
-    hPutStr stderr $ "Message packed into " <> showBytes (L.length packed)
-    packTime <- getCurrentTime
-    hPutStrLn stderr $ " in " <> show (diffUTCTime packTime sampleTime)
-
-    hPutStrLn stderr $ "Packing speed: " <> showSpeed (L.length packed) (realToFrac (diffUTCTime packTime sampleTime))
-
-    L.putStr packed
+main = generate pack

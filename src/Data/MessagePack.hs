af4d45cfa63581838ff4d8a4b0d0e60517f01aac
iphydf <iphydf@users.noreply.github.com>
2023-10-14 21:54:08 +0000

    chore: Update to new third_party method for haskell deps.

--- src/Data/MessagePack.hs
+++ src/Data/MessagePack.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE CPP              #-}
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE Trustworthy      #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
 --------------------------------------------------------------------
 -- |
